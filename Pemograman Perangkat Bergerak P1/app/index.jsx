9f8fc0169c85428ee623fe9188e85e83a3f4fd6a
STEVENEMMANUEL_242310002 <191113987+Steven-Emmanuel@users.noreply.github.com>
2026-06-28 19:19:07 +0700

    Modul 1

---
+++ Pemograman Perangkat Bergerak P1/app/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function index() {
+    return <Redirect href=" /modules/Latihan1" />;
+}
---
+++ Pemograman Perangkat Bergerak P1/app/module latihan/latihan1.jsx
@@ -0,0 +1,10 @@
+import { Status Bar } from 'expo-status-bar'
+import React from 'react'
+import { Stack } from 'expo-eouter'
+
+export default function Latihan1() {
+    return (
+        <><StatusBar sytle="auto" />
+        <Stack screenOptions={{ headerShown: false }} /></>
+    ),
+}
